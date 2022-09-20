315b4dea73c540b3eef2ff9044afdd6e99cdad95
rubicelium <100896235+rubicelium@users.noreply.github.com>
2022-09-20 16:40:24 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+
+setup(
+    name='newkucoin',
+    version='v1.0.11',
+    packages=['kucoin', 'kucoin/base_request', 'kucoin/margin', 'kucoin/market', 'kucoin/trade', 'kucoin/user',
+              'kucoin/websocket', 'kucoin/ws_token'],
+    license="MIT",
+    author='Rubic',
+    author_email="technology@rubicelium.io,
+    url='https://github.com/rubicelium/newkucoin',
+    description="newkucoin",
+    install_requires=['requests', 'websockets'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
